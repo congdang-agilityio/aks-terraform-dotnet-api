c26fda14a0d7305df85bb3f80604dd256ae00bcc
Cong Dang <congdang@asnet.com.vn>
2022-05-20 13:49:16 +0700

    setting node group

--- infrastructure/eks-terraform/main.tf
+++ infrastructure/eks-terraform/main.tf
@@ -33,19 +33,86 @@ module "congdang-cluster" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = "congdang-cluster"
   cluster_version = "1.17"
-  subnets         = ["subnet-2afc2062", "subnet-4c4ee22a"] #CHANGE # Donot choose subnet from us-east-1e
+  subnet_ids         = ["subnet-2afc2062", "subnet-4c4ee22a"] #CHANGE # Donot choose subnet from us-east-1e
   #subnets = data.aws_subnet_ids.subnets.ids
   vpc_id          = aws_default_vpc.default.id
   #vpc_id         = "vpc-1234556abcdef"
 
-  node_groups = [
-    {
-      instance_type = "t2.micro"
-      max_capacity  = 5
-      desired_capacity = 3
-      min_capacity  = 3
+  # node_groups = [
+  #   {
+  #     instance_type = "t2.micro"
+  #     max_capacity  = 5
+  #     desired_capacity = 3
+  #     min_capacity  = 3
+  #   }
+  # ]
+
+  # Self Managed Node Group(s)
+  self_managed_node_group_defaults = {
+    instance_type                          = "t2.micro"
+    update_launch_template_default_version = true
+    iam_role_additional_policies = [
+      "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+    ]
+  }
+
+  self_managed_node_groups = {
+    one = {
+      name         = "mixed-1"
+      max_size     = 5
+      desired_size = 2
+
+      use_mixed_instances_policy = true
+      mixed_instances_policy = {
+        instances_distribution = {
+          on_demand_base_capacity                  = 0
+          on_demand_percentage_above_base_capacity = 10
+          spot_allocation_strategy                 = "capacity-optimized"
+        }
+
+        override = [
+          {
+            instance_type     = "t2.micro"
+            weighted_capacity = "1"
+          },
+          {
+            instance_type     = "t2.micro"
+            weighted_capacity = "2"
+          },
+        ]
+      }
     }
-  ]
+  }
+
+  # EKS Managed Node Group(s)
+  eks_managed_node_group_defaults = {
+    disk_size      = 50
+    instance_types = ["t2.micro"]
+  }
+
+  eks_managed_node_groups = {
+    blue = {}
+    green = {
+      min_size     = 1
+      max_size     = 10
+      desired_size = 1
+
+      instance_types = ["t2.micro"]
+      capacity_type  = "SPOT"
+    }
+  }
+
+  # Fargate Profile(s)
+  fargate_profiles = {
+    default = {
+      name = "default"
+      selectors = [
+        {
+          namespace = "default"
+        }
+      ]
+    }
+  }
 }
 
 data "aws_eks_cluster" "cluster" {
